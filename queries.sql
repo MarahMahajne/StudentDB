eaf3440c5a5736ca45fb69f6e44346e39864f98b
marah <marahmahajne68@gmail.com>
2024-05-21 23:59:19 +0300

    add files

---
+++ queries.sql
@@ -0,0 +1,8 @@
+
+-- Some SELECT queries
+
+SELECT * FROM students;
+
+SELECT * FROM courses;
+
+SELECT * FROM enrollments;
---
+++ run_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update and install MySQL server.
+echo "Installing MySQL server..."
+sudo apt-get update
+sudo apt-get install -y mysql-server
+
+# Start MySQL service.
+echo "Starting MySQL service..."
+sudo service mysql start
+
+# Run SQL scripts
+echo "Setting up databases and tables..."
+sudo mysql -u root << EOF
+SOURCE create_table.sql;
+SOURCE insert_data.sql;
+SOURCE queries.sql;
+SOURCE data_manipulation.sql;
+EOF
+
+echo "Setup complete! MySQL has been installed and database is set up."
